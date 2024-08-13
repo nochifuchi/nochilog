14a831aed8882d5bba7367d6a7325daa26735a86
Fumitaka Nozawa <nozawa@bmc-net.jp>
2024-08-13 11:01:50 +0900

    add site.config.ts

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,11 @@
+import { siteConfig } from '@/site.config';
+
 const Footer = () => {
   return (
     <footer className="text-center bg-gray-900 text-white w-screen block">
       <div className="container px-6 flex flex-col items-center max-w-2xl w-full mx-auto">
         <div className="text-center p-4">
-          <small>© sample site</small>
+          <small>&copy; {siteConfig.title}</small>
         </div>
       </div>
     </footer>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { siteConfig } from '@/site.config';
 import Link from 'next/link';
 
 const Navbar = () => {
@@ -5,8 +6,8 @@ const Navbar = () => {
     <nav className="relative w-full flex flex-wrap items-center justify-between py-3  text-gray-500 hover:text-gray-700 focus:text-gray-700 navbar navbar-expand-lg navbar-light w-screen">
       <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
         <div className="bg-grey-light rounded-md w-full" aria-label="breadcrumb">
-          <Link href="/">
-            <div className="text-gray-500 hover:text-gray-600">sample site</div>
+          <Link href="/" className="text-gray-500 hover:text-gray-600">
+            {siteConfig.title}
           </Link>
           {/* Breadcrumb */}
         </div>
