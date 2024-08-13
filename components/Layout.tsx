67e640150af54f3850358ca99269bf08ccf5f3f9
Fumitaka Nozawa <nozawa@bmc-net.jp>
2024-08-13 12:00:13 +0900

    add children to Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,14 @@
+import type { LayoutProps } from '@/types/types';
+import type { FC } from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
-const Layout = () => {
+const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="relative overflow-hidden">
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
         <Navbar />
-        <main></main>
+        <main className="w-full pb-12 px-4">{children}</main>
         <Footer />
       </div>
     </div>
