83a728730c63bfd45fdbe7ce1ff18b83733bd920
Fumitaka Nozawa <nozawa@bmc-net.jp>
2024-08-13 12:21:38 +0900

    add sample card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
+import Card from '@/components/Card';
 import Layout from '@/components/Layout';
 import { siteConfig } from '@/site.config';
+import { sampleCards } from '@/utils/sample';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div className="pt-12">
+      <div className="w-full pt-12">
         <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
-        <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">{/* Card */}</div>
+        <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
+          {sampleCards.map((page, index) => (
+            <Card key={index} page={page} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
