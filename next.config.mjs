84bfb814f1322cdc7338633fec12a4b34305a728
Fumitaka Nozawa <nozawa@bmc-net.jp>
2024-08-18 12:20:51 +0900

    add images domain to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'www.notion.so',
+      },
+      {
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
