c4b571c48536ee0fefb88ddadfa96aed1b119e34
Fumitaka Nozawa <nozawa@bmc-net.jp>
2024-09-01 11:16:54 +0900

    fix Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,12 +5,10 @@ import Navbar from './Navbar';
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="relative overflow-hidden">
-      <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
-        <Navbar />
-        <main className="w-full pb-12 px-4">{children}</main>
-        <Footer />
-      </div>
+    <div>
+      <Navbar />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
     </div>
   );
 };
