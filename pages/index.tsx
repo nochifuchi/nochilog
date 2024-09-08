7ab12bde5936d926e925ad6c6a82b35fbfd1f44b
Fumitaka Nozawa <nozawa@bmc-net.jp>
2024-09-08 10:06:34 +0900

    update layout style

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,13 +18,13 @@ export const getStaticProps: GetStaticProps = async () => {
 const Home: NextPage<IndexProps> = ({ pages }) => {
   return (
     <Layout>
-      <div className="flex flex-col md:flex-row max-w-screen-lg lg:mx-auto px-2 lg:px-0 ">
-        <section className="grid md:gap-6 md:grid-cols-2 w-full">
+      <div className="max-w-screen-md mx-auto pb-12 sm:pb-16 px-4 lg:px-0 ">
+        <section className="grid gap-y-6 sm:gap-6 sm:grid-cols-2 w-full">
           {pages.map((page, index) => (
             <Card key={index} page={page} />
           ))}
         </section>
-        <section className="ml-0 md:ml-8 mb-7 md:md-0 shrink-0 md:w-64">サイド</section>
+        {/* <section className="ml-0 md:ml-8 mb-7 md:md-0 shrink-0 md:w-64">サイド</section> */}
       </div>
     </Layout>
   );
